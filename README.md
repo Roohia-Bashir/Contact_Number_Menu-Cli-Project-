d25c63e09226fbc33ec7a34dc40384a64f782d59
Roohia-Bashir <160015350+Roohia-Bashir@users.noreply.github.com>
2024-05-31 03:34:38 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# Contact_Number_Menu-Cli-Project-
-CLI project to manage contact numbers with an intuitive menu interface.
+# Contact Numbers Menu CLI
+
+A simple and intuitive CLI application to manage contact numbers. This project uses `inquirer` for interactive prompts, `chalk` for colorful terminal output, and `figlet` for stylized text headings.
+
+## Features
+
+- **Add Contact**: Add a new contact with a name and phone number.
+- **View Contacts**: View all the contacts stored in the application.
+- **Close Menu**: Exit the application.
+
+## Installation
+
+To use this CLI application, you need to have Node.js installed. You can install the application globally using `npm`:
+
+npm install 04-rsib_contact-numbers-menue_cli
+Alternatively, you can use npx to run the CLI without installing it globally:
+
+Copy code
+npx 04-rsib_contact-numbers-menue_cli
+
+Add Contact: Follow the prompts to enter the contact's name and phone number.
+View Contacts: Displays a list of all stored contacts.
+Close Menu: Exits the application.
+
+License
+This project is licensed under the MIT License.
+
+Github link:
+
+Copy code
+`https://github.com/Roohia-Bashir`
---
+++ index.js
@@ -0,0 +1,58 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+import figlet from "figlet";
+// Display the main heading with figlet and chalk
+console.log(chalk.green(figlet.textSync("Contact Menu", {
+    font: 'Standard',
+    horizontalLayout: 'default',
+    verticalLayout: 'default'
+})));
+console.log(chalk.yellowBright("Welcome to Contact Numbers Menu"));
+let contacts = [];
+let contactsSerialNo = 1;
+async function contactMenuInput() {
+    const inputContact = await inquirer.prompt({
+        type: 'list',
+        name: 'contact',
+        message: "Select your option",
+        choices: ["Add Contact", "View Contacts", "Close Menu"]
+    });
+    let { contact } = inputContact; // Destructuring
+    if (contact === "Add Contact")
+        addContact();
+    if (contact === "View Contacts")
+        viewContact();
+    if (contact === "Close Menu") {
+        console.log(chalk.magentaBright(`\nThank You for using contact menu!`));
+        process.exit(0); // Exit the process to stop further input
+    }
+}
+contactMenuInput();
+async function addContact() {
+    const inputContactDetails = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'personName',
+            message: chalk.blueBright(`Enter Person Name!`)
+        },
+        {
+            type: 'number',
+            name: 'phoneNumber',
+            message: chalk.blueBright(`Enter Contact Number!`)
+        }
+    ]);
+    const { personName, phoneNumber } = inputContactDetails; // Destructuring
+    contacts.push({ ID: contactsSerialNo++, Name: personName, PhoneNo: phoneNumber });
+    console.log(chalk.greenBright.bold(`\nNew Contact number has been added\n`));
+    contactMenuInput();
+}
+function viewContact() {
+    if (contacts.length > 0) {
+        contacts.forEach((user) => console.log(chalk.green(`\n${user.ID}. Person Name: ${chalk.cyan(user.Name)} ---- Contact Number: ${chalk.yellow(user.PhoneNo)}\n`)));
+    }
+    else {
+        console.log(chalk.red(`\nNo Contacts available!`));
+    }
+    contactMenuInput();
+}
